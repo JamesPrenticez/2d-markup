63f46a13de931e91ccf2839f7a3d2599f867eb5c
James Prentice <jamesprenticez@gmail.com>
2024-06-13 20:31:52 +1200

    Initial Commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+function App() {
+
+  return (
+  // <div className="bg-gradient-to-tr from-[#6D26FF] via-[#0047BA] to-[#002470] min-h-screen text-gray-50">
+  <div className="bg-gradient-to-r from-red-500 to-orange-500 min-h-screen text-gray-50 flex">
+    <div className="flex flex-col items-center grow bg-black/20">
+
+      <div className="bg-white/5 max-w-7xl w-full flex-col grow">
+        <h1 className="text-3xl font-semibold">Hello World</h1>
+
+      </div>
+
+    </div>
+  </div>
+  )
+}
+
+export default App
